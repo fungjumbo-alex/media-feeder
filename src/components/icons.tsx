7570239a8dd85de2b88ac7720813382e55b48a9a
alefung <alex.sc.fung@gmail.com>
2025-12-03 08:44:14 +0000

    fix: invalid SVG polyline points in ZapOffIcon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -250,9 +250,9 @@ export const SettingsIcon = (props: React.SVGProps<SVGSVGElement>) => (
 export const ZapOffIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
         <polyline points="12.41 6.75 13 2 10.57 4.92" />
-        <polyline points="18.57 12.91 21 10h-5.34" />
-        <polyline points="8 8 3 14h5.34" />
-        <polyline points="11.59 17.25 11 22l2.43-2.92" />
+        <polyline points="18.57 12.91 21 10 15.66 10" />
+        <polyline points="8 8 3 14 8.34 14" />
+        <polyline points="11.59 17.25 11 22 13.43 19.08" />
         <line x1="1" y1="1" x2="23" y2="23" />
     </svg>
 );
